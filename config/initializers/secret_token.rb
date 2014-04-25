7a4c37a9564da17b04587149d7bacc1de834b5ee
sanjeevs <snjvsingh123@gmail.com>
2014-04-25 13:54:57 -0400

    initial version

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Upanishad1::Application.config.secret_token = 'f69b651905a603e6f58f1f021d05a0bb1f77bb9749dfbcade5be606f20aa1d1dd318a91ffa12bb9905af9ae7ae5bf41386abaafb66eebfb7316de9ca38094b6e'
